07f0fc968441e15f4e9ee928bd4a1e7bc1270de0
Harran Williams <hayron24@gmail.com>
2016-07-12 23:16:14 -0400

    introduced typology and site responsive design

--- functions.php
+++ functions.php
@@ -105,7 +105,8 @@ function popperscores_scripts() {
 	wp_enqueue_style( 'popperscores-style', get_stylesheet_uri() );
 
 	// Add Google fonts Fire Sans and Merriweather
-	wp_enqueue_style( 'popperscores-google-fonts', 'https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i|Merriweather:400,400i,700,700i' );
+	// wp_enqueue_style( 'popperscores-google-fonts', 'https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i|Merriweather:400,400i,700,700i' );
+	wp_enqueue_style( 'popperscores-local-fonts', get_template_directory_uri() . '/fonts/custom-fonts.css'); 
 
 	wp_enqueue_script( 'popperscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
